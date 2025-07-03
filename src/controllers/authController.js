f0665a9c8a504ead07702ba24b540ee534f2b605
Meo <Meo@gmail.com>
2025-07-03 14:34:56 +0700

    fix role

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,7 +6,7 @@ const generateAccessToken = (user) => {
   return jwt.sign(
     { userId: user._id, role: user.role, name: user.name },
     process.env.JWT_SECRET,
-    { expiresIn: '15m' } 
+    { expiresIn: '15m' }
   );
 };
 const generateRefreshToken = (user) => {
@@ -19,17 +19,17 @@ const generateRefreshToken = (user) => {
 
 exports.register = async (req, res) => {
   try {
-    const { name, email, password,phone,role,gender } = req.body;
+    const { name, email, password, phone, role, gender } = req.body;
 
     const existingUser = await User.findOne({ email });
     if (existingUser) return res.status(400).json({ message: 'Email already has been used' });
 
-    const newUser = new User({ name, email, password,phone,role,gender });
+    const newUser = new User({ name, email, password, phone, role, gender });
     await newUser.save();
 
     res.status(201).json({ message: 'Register successfully' });
   } catch (err) {
-     console.error(err);
+    console.error(err);
     res.status(500).json({ message: 'Server error' });
   }
 };
@@ -49,16 +49,17 @@ exports.login = async (req, res) => {
     // Tạo JWT
     const accessToken = generateAccessToken(user);
     const refreshToken = generateRefreshToken(user);
-  res.status(200).json({
-  message: 'Login successfully',
-  accessToken, 
-  refreshToken,
-  user: {
-    id: user._id,
-    name: user.name,
-    email: user.email
-  }
-});
+    res.status(200).json({
+      message: 'Login successfully',
+      accessToken,
+      refreshToken,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role, // dồng được thêm trong hàm login ở file authController.js
+      }
+    });
 
   } catch (err) {
     res.status(500).json({ message: 'Server error' });
@@ -71,9 +72,9 @@ exports.refreshToken = (req, res) => {
   jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
     if (err) return res.status(403).json({ message: 'Invalid token' });
     const newAccessToken = generateAccessToken({ _id: decoded.userId });
-    return res.json({ 
-      accessToken: newAccessToken, 
-      expiresIn: 30 * 1000 
+    return res.json({
+      accessToken: newAccessToken,
+      expiresIn: 30 * 1000
     });
   });
 };

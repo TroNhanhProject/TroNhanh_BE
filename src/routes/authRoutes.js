5b34e7d1fafa960bdc8150537c9df060004141c3
khanhluan12 <155692902+khanhluan12@users.noreply.github.com>
2025-07-07 17:49:47 +0700

    Add email verification and password reset features
    Implemented OTP-based email verification and password reset functionality. Added UserOTP model, updated User model with verification and reset fields, and extended authController and authRoutes to support sending, verifying, and resending OTPs, as well as password reset via email. Updated dependencies to include nodemailer and crypto.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,4 +5,9 @@ const{registerValidator,loginValidator} = require('../middleware/authValidator')
 router.post('/register',registerValidator, authController.register);
 router.post('/login',loginValidator, authController.login);
 router.post('/refresh-token', authController.refreshToken);
+router.post('/send-otp',authController.sendOTP);
+router.post('/verify-otp',authController.verifyOTP);
+router.post('/resend-otp', authController.resendOTP);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password/:token', authController.resetPassword);
 module.exports = router;

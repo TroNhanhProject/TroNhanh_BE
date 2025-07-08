5f8a2286847717def29dc3a5506bc1a92aa0d49c
khanhluan12 <155692902+khanhluan12@users.noreply.github.com>
2025-07-08 18:51:31 +0700

    Add reset password validation middleware
    Introduced resetPasswordValidator to enforce strong password requirements during password reset. Updated the reset password route to use this validator. Also added a new avatar image to uploads.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const{registerValidator,loginValidator} = require('../middleware/authValidator')
+const{registerValidator,loginValidator,resetPasswordValidator} = require('../middleware/authValidator')
 router.post('/register',registerValidator, authController.register);
 router.post('/login',loginValidator, authController.login);
 router.post('/refresh-token', authController.refreshToken);
@@ -9,5 +9,5 @@ router.post('/send-otp',authController.sendOTP);
 router.post('/verify-otp',authController.verifyOTP);
 router.post('/resend-otp', authController.resendOTP);
 router.post('/forgot-password', authController.forgotPassword);
-router.post('/reset-password/:token', authController.resetPassword);
+router.post('/reset-password/:token',resetPasswordValidator, authController.resetPassword);
 module.exports = router;

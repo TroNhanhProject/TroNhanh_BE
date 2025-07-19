28c000d42205759343224f2ba8756024062f1b7d
Meo <Meo@gmail.com>
2025-07-19 19:06:40 +0700

    update-user-isMembership

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -2,11 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const { createPaymentUrl, vnpayReturn, testUpdateAccommodation } = require('../controllers/vnpayController');
-const { getCurrentMembershipOfUser } = require('../controllers/paymentController');
+const { getCurrentMembershipOfUser, updateAllUsersMembershipStatus } = require('../controllers/paymentController');
 
 router.post('/create', createPaymentUrl); // POST /api/payment/create
 router.get('/vnpay_return', vnpayReturn);
 router.post('/test-update', testUpdateAccommodation); // Test route
 router.get('/current/:userId', getCurrentMembershipOfUser);
+router.post('/update-all-membership-status', updateAllUsersMembershipStatus); // Admin endpoint
 
 module.exports = router;

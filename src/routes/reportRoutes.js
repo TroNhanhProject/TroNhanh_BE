ea89e8fb1aad40a7c20a9bcd1293299ae180f199
khanhluan12 <155692902+khanhluan12@users.noreply.github.com>
2025-07-20 00:59:40 +0700

    update report customer

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -6,5 +6,5 @@ const authMiddleware = require("../middleware/authMiddleWare");
 router.post("/", authMiddleware, reportController.createReport);
 router.get("/my-reports", authMiddleware, reportController.getMyReports);
 router.get('/owners', authMiddleware, reportController.getOwners);
-
+router.get('/check-history',authMiddleware,reportController.checkBookingHistory);
 module.exports = router;
